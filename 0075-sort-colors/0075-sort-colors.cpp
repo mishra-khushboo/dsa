0f6f04db19da9391c62588a5bab1ced8fad10179
mishra-khushboo <khush27093@gmail.com>
2025-08-23 23:08:19 +0530

    Time: 0 ms (100%), Space: 11.8 MB (12.06%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c0 = 0, c1 = 0, c2 = 0;
-        int n = nums.size();
+        
+    //     int c0 = 0, c1 = 0, c2 = 0;
+    //     int n = nums.size();
 
-        for(int elm : nums){
-            if(elm == 0){
-                c0++;
-            }else if(elm == 1){
-                c1++;
-            }else{
-                c2++;
-            }
+    //     for(int elm : nums){
+    //         if(elm == 0){
+    //             c0++;
+    //         }else if(elm == 1){
+    //             c1++;
+    //         }else{
+    //             c2++;
+    //         }
+    //     }
+    //     int idx = 0;
+    //     while(c0--){
+    //         nums[idx++] = 0;
+    //     }
+    //     while(c1--){
+    //         nums[idx++] = 1;
+    //     }
+    //     while(c2--){
+    //         nums[idx++] = 2;
+    //     }
+
+    int n = nums.size();
+    int low = 0, mid = 0, high = n - 1;
+    while(mid <= high){
+        if(nums[mid] == 0){
+            swap(nums[mid++], nums[low++]);
+        }else if(nums[mid] == 1){
+            mid++;
+        }else{
+            swap(nums[mid], nums[high--]);
         }
-        int idx = 0;
-        while(c0--){
-            nums[idx++] = 0;
-        }
-        while(c1--){
-            nums[idx++] = 1;
-        }
-        while(c2--){
-            nums[idx++] = 2;
-        }
+    }
+
     }
 };
