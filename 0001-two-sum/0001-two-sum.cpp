bb3fe1ccd66fbf77338f0163ee65d1024c135fde
mishra-khushboo <khush27093@gmail.com>
2025-10-16 23:06:26 +0530

    Time: 3 ms (69.35%), Space: 14.8 MB (50.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int tar) {
+        //Time Complexity: O(n)
+        //Space Complexity: O(n)
         unordered_map<int,int>m;
         vector<int>ans;
         int n = nums.size();
@@ -9,8 +11,8 @@ public:
             int first = nums[i];
             int sec = tar - first;
             if(m.find(sec) != m.end()){
-                ans.push_back(i);
                 ans.push_back(m[sec]);
+                ans.push_back(i);
                 break;
             }
             m[first] = i;
@@ -18,3 +20,13 @@ public:
         return ans;
     }
 };
+
+//Time Complexity: O(n²)
+//Space Complexity: O(1)
+// for (int i = 0; i < nums.size(); i++) {
+//         for (int j = i + 1; j < nums.size(); j++) {
+//             if (nums[i] + nums[j] == target)
+//                 return {i, j};
+//         }
+//     }
+//     return {};
